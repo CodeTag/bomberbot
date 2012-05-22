dd9745ae7d2371cef7d1fda05465177d9866cef7
Giovanny <ekeisco@gmail.com>
2012-05-22 00:26:13 -0500

    ajustes pequeños al server ajustando cosas de protocolo

--- bomberbot.js
+++ bomberbot.js
@@ -1,18 +1,29 @@
 net = require("net")
 
-var validActions=['N','E','S','O','B','P','n','e','s','o','b','p'];
-
+var validActions=['N\r\n','E\r\n','S\r\n','O\r\n','P\r\n','BN\r\n','BE\r\n','BS\r\n','BO\r\n'];
+var playersConnected =[];
+var ponerEnSalaEspera = function(socket){
+	console.log("un nuevo player conectado");
+};
 var server = net.createServer( function(socket){
-        socket.write("welcome bomberbot server\n");
+        socket.write("*****************************************\n"+
+        "* Bienvenido al servidor de Bomberbot\n"+
+        "* Para información del juego entrar a bomberbot.com\n"+
+        "****************************************\n");
+        playersConnected.push(socket);
+        ponerEnSalaEspera(socket);
         socket.on("data", function(data){
-            data = data.toString()[0];
-            var ok =validActions.indexOf(data)!==-1?1:0;
-            socket.write(ok+"\n");
-            
-            if (data==='Q' || data==='q'){
-                socket.end("bye bye\n");
-            }
-
+            data = data.toString().toUpperCase();
+            console.log(data);
+           	var ok =validActions.indexOf(data)!==-1?1:0;
+           	if(ok){
+           		socket.write("bingo\r\n");
+		       	for(var i = 0; i<playersConnected.length; i++){
+		       		if(socket!==playersConnected[i]){
+		       			playersConnected[i].write("otro lo hizo\r\n");
+		       		}
+		       	}
+           	}
         });
        
     }
